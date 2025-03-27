7e122048db358255522a3d46667126f9cdd35aea
Ariel-250225 <seobin250225@icloud.com>
2025-03-27 22:17:59 +0700

    feature : project build 파일 추가

---
+++ vite.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true,
+    proxy: {
+      // 첫 번째 프록시 설정
+      "/api": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+        secure: false,
+        ws: true,
+      },
+      // 두 번째 프록시 설정
+      "/game": {
+        target: "http://localhost:8050",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/game/, ""),
+        secure: false,
+        ws: true,
+      },
+      "/streaming": {
+        target: "http://localhost:8070",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/streaming/, ""),
+        secure: false,
+        ws: true,
+      },
+    },
+  },
+});

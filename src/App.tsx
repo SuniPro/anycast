eeb2c792819519545f6894d62fe4b1b168944823
Ariel-250225 <seobin250225@icloud.com>
2025-03-29 21:23:05 +0700

    feature : 불필요파일 제거

--- src/App.tsx
+++ src/App.tsx
@@ -7,19 +7,10 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { SearchContextProvider } from "./Context/SearchContext";
 import { Auditorium } from "./page/Auditorium";
 import { Toaster } from "react-hot-toast";
-import styled from "@emotion/styled";
-import { slideUp } from "./components/styled/Animation/Animation";
 
 const QUERY_CLIENT = new QueryClient();
 
 function App() {
-  // const [check, setCheck] = useState(() => hasIntroCookie());
-  //
-  // const handleIntroCheck = () => {
-  //   setIntroCookie();
-  //   setCheck(true);
-  // };
-
   return (
     <QueryClientProvider client={QUERY_CLIENT}>
       <WindowContextProvider>
@@ -37,9 +28,5 @@ function App() {
     </QueryClientProvider>
   );
 }
-
-export const SlideInContent = styled.div`
-  animation: ${slideUp} 0.6s ease-out forwards;
-`;
 
 export default App;

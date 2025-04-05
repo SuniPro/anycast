e99ad3eb4c91ef89c5bf70c5d821a915717bde66
Ariel-250225 <seobin250225@icloud.com>
2025-04-05 16:03:05 +0700

    feature : cicd 파이프라인 구축, nginx 에러 핸들링, 호스팅 서버 반영완료

--- vite.config.ts
+++ vite.config.ts
@@ -8,23 +8,8 @@ export default defineConfig({
     port: 5170,
     proxy: {
       // 첫 번째 프록시 설정
-      "/api": {
-        target: "http://localhost:8080",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-        secure: false,
-        ws: true,
-      },
-      // 두 번째 프록시 설정
-      "/game": {
-        target: "http://localhost:8050",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/game/, ""),
-        secure: false,
-        ws: true,
-      },
       "/streaming": {
-        target: "http://192.168.9.108:8070",
+        target: "http://13.214.147.245:8070",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/streaming/, ""),
         secure: false,

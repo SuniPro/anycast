b03085276b6a626433ada0d6f5e880db47c9f3c4
Ariel-250225 <seobin250225@icloud.com>
2025-04-08 11:33:22 +0700

    refactor : SOOP 버퍼링 경고 삭제, google tag manager 추가

--- src/page/Auditorium.tsx
+++ src/page/Auditorium.tsx
@@ -5,7 +5,7 @@ import { getSportsStreamById } from "../api/streaming";
 import { useLocation, useNavigate, useParams } from "react-router-dom";
 import { SportsLeagueType } from "../model/Streams";
 import { useEffect, useState } from "react";
-import { ErrorAlert, SuccessAlert } from "../components/Alert/Alerts";
+import { ErrorAlert } from "../components/Alert/Alerts";
 import { useTheme } from "@emotion/react";
 
 export function Auditorium() {
@@ -25,11 +25,6 @@ export function Auditorium() {
         setLeagueInfo(r);
       }
     });
-    if (leagueInfo?.streamUrl.includes("soop")) {
-      SuccessAlert(
-        `SOOP은 현재 서버가 불안정하여 약 1분간 잦은 버퍼링이 발생할 수 있습니다. \n 1분 후 정상 재생되니 이점 참고 바랍니다.`,
-      );
-    }
   }, [id, leagueInfo?.streamUrl, location, navigate]);
 
   if (!leagueInfo) return;
